ac813625c8aa5da5c3aa630847185dd9f15023e1
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-04-06 12:21:44 +0300

    arithmetic operators update

--- 01-variables-data-types/07-arithmetic-operators-math-functions/index.php
+++ 01-variables-data-types/07-arithmetic-operators-math-functions/index.php
@@ -9,6 +9,43 @@
 | `/`      | Division       |
 | `%`      | Modulus        |
 */
+$output = null;
+
+$n1 = 20;
+$n2 = 19;
+
+$output = "$n1 + $n2 = ". $n1 + $n2;
+$output = "$n1 - $n2 = ". $n1 - $n2;
+$output = "$n1 * $n2 = ". $n1 * $n2;
+$output = "$n1 / $n2 = ". $n1 / $n2;
+$output = "$n1 % $n2 = ". $n1 % $n2;
+
+// assignment operator
+$n3 = 10;
+$n3 += 13;
+
+// built int PHP functions
+
+// $output = $rand(1, 1000);
+// $output = $getrandmax(1, 1000);
+
+// round()
+// $output = $round(1.4);
+
+// sqrt()
+$output = sqrt(55);
+
+// abs()
+$output = abs(-55);
+
+// min ()
+// max()
+
+$output= number_format(111111.2222, 2, '.', ',')
+
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -29,6 +66,9 @@
   <div class="container mx-auto p-4 mt-4">
     <div class="bg-white rounded-lg shadow-md p-6 mt-6">
       <!-- Output -->
+      <p class="text-xl">
+        <?= $output ?>
+      </p>
     </div>
   </div>
 </body>
