8c6634313f7445eb0b958d8ef39a88b8f4ea2d44
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-04-06 09:36:52 +0300

    data types update

--- 01-variables-data-types/03-data-types/index.php
+++ 01-variables-data-types/03-data-types/index.php
@@ -12,3 +12,44 @@
 - NULL
 - Resource
 */
+// string
+$name = "Ocen";
+$name1 = "Aaron";
+
+var_dump($name);
+echo "<br/>";
+// echo gettype($name1);
+
+// integer
+$age = 33;
+var_dump($age);
+echo "<br/>";
+
+// float
+$rating = 4.4;
+var_dump($rating);
+echo "<br/>";
+
+// boolean
+$isLoaded = true;
+var_dump($isLoaded);
+echo "<br/>";
+
+// array
+$friends = ['ocen', 33, 'peter'];
+var_dump($friends);
+echo "<br/>";
+
+// object
+$person = new stdClass();
+var_dump($person);
+echo "<br/>";
+
+// null
+$car = null;
+var_dump($car);
+echo "<br/>";
+
+// resource
+$file = fopen('sample.txt', 'r');
+var_dump($file);
